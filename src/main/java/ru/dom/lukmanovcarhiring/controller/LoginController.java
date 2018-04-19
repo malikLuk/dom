fb70be37c2d583e3e14025d60f1cc103590f3eb5
MLukmanov <MLukmanov@IBS.RU>
2018-04-19 19:47:34 +0500

    still need to add security

---
+++ src/main/java/ru/dom/lukmanovcarhiring/controller/LoginController.java
@@ -0,0 +1,17 @@
+package ru.dom.lukmanovcarhiring.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class LoginController {
+
+    @RequestMapping(value = "/login")
+    public ModelAndView login() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("login");
+        return modelAndView;
+    }
+
+}
