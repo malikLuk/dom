b276d4b15011b5df485fd832feee403f14678ed8
MLukmanov <MLukmanov@IBS.RU>
2018-04-27 17:29:55 +0500

    сделать аренду и по возможности wildcard для раздачи эвентов. Мб сработает через return func

--- src/main/resources/static/js/location.js
+++ src/main/resources/static/js/location.js
@@ -1,18 +1,13 @@
 $(document).ready(function () {
 
     $("[id^=get_car_list_]").on('click', function (event) {
-        var currentId = event.currentTarget.id.replace('get_car_list_', '');
-        go(event, currentId);
+        var currentId = event.currentTarget.id.replace('get_car_list_', '');  //вырезаем из подстроки нр get_car_list_1 get_car_list_ и
+        go(event, currentId);                                                 // оставляем только номер
     });
-
-    var a = $("#kek");
-    a.append('<img th:src="@{\'/img/audi.jpg\'}"/>');
 
 });
 
 function go(event, currentId) {
-    var me = this;
-    debugger;
     event.preventDefault();
     var headers = {};
     var token = $("meta[name='_csrf']").attr("content");
@@ -36,19 +31,21 @@ function go(event, currentId) {
             var carPlace = $('#car_place');
             data.forEach(function (value, index) {
 
-                $('<div class="thumbnail" id="car_thumbnail_' + index + '">').append('<img src="/img/' + value.name +'.jpg" />').appendTo(carPlace);
+                $('<div class="thumbnail" id="car_thumbnail_' + index + '">').append('<img src="/img/' + value.name + '.jpg" />').appendTo(carPlace);
 
                 var carThumbnail = $('#car_thumbnail_' + index);
                 carThumbnail.append('<div class="caption" id="car_caption_' + index + '">');
 
                 var carCaption = $('#car_caption_' + index);
-                carCaption.append('<h3>qqqqqq</h3>').append('<p>qwwwwwwwwwww</p>')
+                carCaption.append('<h3>' + value.name + '</h3>').append('<p>' + value.status + '</p>')
+
+                $('<button class="myButton" id="my_btn_id_' + index + '">hire</button>').appendTo(carCaption);
 
             });
 
-
-
-
+            wildcardEvent("my_btn_id_", "click", function () {
+                return hire()
+            });
 
         },
         error: function () {
@@ -57,6 +54,12 @@ function go(event, currentId) {
     });
 }
 
-function clear(element) {
-    element.innerHTML = "";
+function hire() {
+
+}
+
+function wildcardEvent(idTemplate, eventType, func) {
+    $("[id^=" + idTemplate + "]").on(eventType, function (event) {
+        func();
+    });
 }
